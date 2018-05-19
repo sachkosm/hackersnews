74b3dae968f20270ec5e3174059ac07f8ee7e9cb
si180@yahoo.com <si180@yahoo.com>
2018-05-19 09:21:23 -0400

    with ssl

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ export default {
 //Deployment Note:
 //Modify in package.json "homepage": "http://localhost:3000"
         DataURL: "https://hn.algolia.com/api/v1/search",
-        ItemURL: "http://hn.algolia.com/api/v1/items/"      //:id  //tags=comment
+        ItemURL: "https://hn.algolia.com/api/v1/items/"      //:id  //tags=comment
 
 }
